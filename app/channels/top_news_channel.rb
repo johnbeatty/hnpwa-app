772d67fe27a14412f6475db1e590b96d714cc362
John Beatty <john@thebeattys.org>
2018-12-01 15:54:04 -0500

    removing extra comments

--- app/channels/top_news_channel.rb
+++ app/channels/top_news_channel.rb
@@ -1,7 +1,5 @@
 class TopNewsChannel < ApplicationCable::Channel
   def subscribed
-    logger.debug "Subscribed to TopNewsChannel from connect class #{params[:top_item_id]}"
-    logger.debug params
     stream_from "TopNewsChannel#{params[:top_item_id]}"
   end
 
