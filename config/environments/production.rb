b9eab4810dfe8ee224458f23a04dfd841a4a32e7
John Beatty <john@thebeattys.org>
2019-01-14 14:34:05 -0500

    More websocket fun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://hnpwa.johnbeatty.co/cable'
   config.action_cable.allowed_request_origins = ['https://hnpwa.johnbeatty.co']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
