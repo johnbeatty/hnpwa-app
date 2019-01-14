506273e8ba6510996bdc3fcc2f106368a24cf888
John Beatty <john@thebeattys.org>
2019-01-14 15:27:21 -0500

    Updating schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,12 @@
 env :PATH, ENV['PATH']
 
-every :minute do
+every 1.minute do
   runner 'LoadTopItemsJob.perform_later'
-  runner 'LoadShowItemsJob.perform_later'
   runner 'LoadNewItemsJob.perform_later'
+end
+
+every 5.minute do 
+  runner 'LoadShowItemsJob.perform_later'
   runner 'LoadJobItemsJob.perform_later'
   runner 'LoadAskItemsJob.perform_later'
 end
