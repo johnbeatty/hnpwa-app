3b3d57efaaba4ddba5bd6c04beeff1995cb7acee
John Beatty <john@thebeattys.org>
2019-01-15 15:46:05 -0500

    cleaning up tops_controller

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,14 +1,9 @@
 class TopsController < ApplicationController
 
   def show
-
     @page = params[:page] ? params[:page].to_i : 0
     @top_item = TopItem.order(:updated_at).last
     @top_items = TopItem.order(:location).limit(ITEMS_PER_PAGE).offset(@page * ITEMS_PER_PAGE).includes(:item)
-
     @total_pages = TopItem.count / ITEMS_PER_PAGE
-
-    # LoadTopItemsJob.perform_later
-
   end
 end
