3c894ddf7d9baaa0636a54fd0da0dca9bb83924d
John Beatty <john@thebeattys.org>
2019-01-17 21:51:36 -0500

    Refactoring out locations from New Items

--- app/channels/new_item_channel.rb
+++ app/channels/new_item_channel.rb
@@ -1,7 +1,7 @@
 class NewItemChannel < ApplicationCable::Channel
   def follow(data)
     stop_all_streams
-    locations = JSON.parse data['locations']
+    locations = data['locations']
     unless locations.nil?
       locations.each do |location|
         stream_from "NewItemChannel:#{location}"
