09877fd82d0472d618d656775164385aea9f874e
John Beatty <john@thebeattys.org>
2019-03-04 10:34:55 -0500

    Slowing down job schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,11 @@
 env :PATH, ENV['PATH']
 
-every 1.minute do
+every 2.minute do
   runner 'LoadTopItemsJob.perform_later'
   runner 'LoadNewItemsJob.perform_later'
 end
 
-every 5.minute do 
+every 10.minute do 
   runner 'LoadShowItemsJob.perform_later'
   runner 'LoadJobItemsJob.perform_later'
   runner 'LoadAskItemsJob.perform_later'
