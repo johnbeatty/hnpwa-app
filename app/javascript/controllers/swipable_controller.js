c41e2ef278064a05ce3ffcd273ea09945c4e6a5f
John Beatty <john@thebeattys.org>
2019-03-05 16:44:04 -0500

    Fixing swipable page

--- app/javascript/controllers/swipable_controller.js
+++ app/javascript/controllers/swipable_controller.js
@@ -2,6 +2,7 @@
 
 const NUMBER_OF_FRAMES = 30;
 const MOVE_PAGE_THRESHOLD = 0.3;
+const STOP_VERTICAL_MOVEMENT_THRESHOLD = 0.02;
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
@@ -9,101 +10,83 @@ export default class extends Controller {
     static targets = ['view']
 
     initialize() {
-        console.log('initialize')
-
-        this.i = 0; 
-        this.x0 = null; 
-        this.locked = false; 
-        this.moving = false;
-        this.w; 
-        this.ini; 
-        this.fin; 
-        this.rID = null; 
-        this.anf;
-        this.n;
-        this.N = 1;
+      this.i = 0; 
+      this.x0 = null; 
+      this.locked = false;
+      this.ini; 
+      this.fin; 
+      this.rID = null; 
+      this.anf;
+      this.n;
     }
 
     connect() {
-        console.log('connect')
+      addEventListener('resize', this.size.bind(this), false);
+      addEventListener('turbolinks:before-cache', this.reset.bind(this), false);
 
-        addEventListener('resize', this.size.bind(this), false);
-        addEventListener('turbolinks:before-cache', this.reset.bind(this), false);
+      this.element.addEventListener('mousedown', this.lock.bind(this), false);
+      this.element.addEventListener('touchstart', this.lock.bind(this), false);
 
-        this.element.addEventListener('mousedown', this.lock.bind(this), false);
-        this.element.addEventListener('touchstart', this.lock.bind(this), false);
+      this.element.addEventListener('mousemove', this.drag.bind(this), false);
+      this.element.addEventListener('touchmove', this.drag.bind(this), false);
 
-        this.element.addEventListener('mousemove', this.drag.bind(this), false);
-        this.element.addEventListener('touchmove', this.drag.bind(this), false);
+      this.element.addEventListener('mouseup', this.move.bind(this) , false);
+      this.element.addEventListener('touchend', this.move.bind(this), false);
 
-        this.element.addEventListener('mouseup', this.move.bind(this) , false);
-        this.element.addEventListener('touchend', this.move.bind(this), false);
-
-        this.size();
-    }
-
-    disconnect() {
-        console.log('disconnect')
+      this.size();
     }
 
     lock(event) {
-      console.log('lock');
-      this.x0 = unify(event).clientX;
-      this.locked = true
+      let initialX = unify(event).clientX;
+      if (initialX < this.quarterWidth 
+        || initialX + this.quarterWidth > this.width ) {
+        this.x0 = initialX;
+        this.locked = true
+      }
     }
 
     drag(event) {
       if(this.locked) {
-          console.log('drag');
+        let dx = unify(event).clientX - this.x0;
+        let f = +(dx/this.width).toFixed(2);
+        let calculation = this.i - f;
+        console.log(`${ calculation } - ${ STOP_VERTICAL_MOVEMENT_THRESHOLD }`);
+        console.log(`${ calculation > STOP_VERTICAL_MOVEMENT_THRESHOLD  } - ${ calculation < -STOP_VERTICAL_MOVEMENT_THRESHOLD }`);
+        this.viewTarget.style.setProperty('--i', `${ calculation }`)
+        if (calculation > STOP_VERTICAL_MOVEMENT_THRESHOLD 
+          || calculation < -STOP_VERTICAL_MOVEMENT_THRESHOLD ) {
+          console.log('stopping event...')
           event.preventDefault();
-          let dx = unify(event).clientX - this.x0;
-          let f = +(dx/this.width).toFixed(2);
-          let calculation = this.i - f;
-          // console.log(`drag dx: ${dx} i: ${this.i} f: ${f} calculation: ${ calculation }`);
-          this.viewTarget.style.setProperty('--i', `${ calculation }`)
-
-          if (!this.moving) {
-            if ( calculation < -MOVE_PAGE_THRESHOLD ) {
-              console.log('move back')
-              this.moving = true;
-              window.history.back();
-            } else if ( calculation > MOVE_PAGE_THRESHOLD) {
-              console.log('move forward')
-              this.moving = true;
-              window.history.forward();
-            }
-          }
         }
+      }
     }
-
 
     move(event) {
+      if(this.locked) {
+        let dx = unify(event).clientX - this.x0;
+        let s = Math.sign(dx); 
+        let f = +(s*dx/this.width).toFixed(2);
+      
+        this.ini = this.i - s*f;
 
-      console.log('move');
-        if(this.locked) {
-          let dx = unify(event).clientX - this.x0;
-          let s = Math.sign(dx); 
-          let f = +(s*dx/this.width).toFixed(2);
-      
-          this.ini = this.i - s*f;
-      
-          if((this.i > 0 || s < 0) && (this.i < this.N - 1 || s > 0) && f > .2) {
-            this.i -= s;
-            f = 1 - f
-          }
-      
-          this.fin = this.i;
-          this.anf = Math.round(f*NUMBER_OF_FRAMES);
-          this.n = 2 + Math.round(f)
-          this.ani();
-          this.x0 = null;
-          this.locked = false;
-          this.moving = false;
+        if ( this.ini < -MOVE_PAGE_THRESHOLD ) {
+          window.history.back();
+        } else if ( this.ini > MOVE_PAGE_THRESHOLD) {
+          window.history.forward();
         }
-    }
-
-    size() {
-        this.width = window.innerWidth;
+        
+        if((this.i > 0 || s < 0) && (this.i < 0 || s > 0) && f > .2) {
+          this.i -= s;
+          f = 1 - f
+        }
+      
+        this.fin = this.i;
+        this.anf = Math.round(f*NUMBER_OF_FRAMES);
+        this.n = 2 + Math.round(f)
+        this.ani();
+        this.x0 = null;
+        this.locked = false;
+      }
     }
 
     stopAni() {
@@ -113,10 +96,7 @@ export default class extends Controller {
 
 
     ani(cf = 0) {
-      // console.log(`ani ini: ${this.ini } fin: ${  this.fin } easeOut: ${ this.easeInOut(cf/this.anf) } cf: ${ cf } anf: ${ this.anf } `);
-      // console.log(`ani ${this.ini + (this.fin - this.ini) * this.easeInOut(cf/this.anf)}`);
-      
-      this.viewTarget.style.setProperty('--i', `${ this.ini + (this.fin - this.ini) * this.easeInOut(cf/this.anf) }`);
+      this.viewTarget.style.setProperty('--i', `${ this.ini + (this.fin - this.ini) * easeInOut(cf/this.anf) }`);
 
       if(cf === this.anf) {
         this.stopAni();
@@ -126,20 +106,20 @@ export default class extends Controller {
       this.rID = requestAnimationFrame(this.ani.bind(this, ++cf))
     }
 
-    bounceOut(k, a = 2.75, b = 1.5) {
-      return 1 - Math.pow(1 - k, a)*Math.abs(Math.cos(Math.pow(k, b)*(this.n + .5)*Math.PI))
-    }
-
-    easeInOut(k) {
-          return .5*(Math.sin((k - .5)*Math.PI) + 1)
+    size() {
+      this.width = window.innerWidth;
+      this.quarterWidth = this.width * MOVE_PAGE_THRESHOLD;
     }
 
     reset() {
-      console.log('reset');
       this.viewTarget.style.setProperty('--i', 0);
     }
 }
 
+function easeInOut(k) {
+  return .5*(Math.sin((k - .5)*Math.PI) + 1);
+}
+
 function unify(event) { 
   return event.changedTouches ? event.changedTouches[0] : event;
-};
+}
