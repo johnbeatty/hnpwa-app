f66c22a33e43f99ae6c3d1dc8650314ea0f8e6b5
John Beatty <john@thebeattys.org>
2019-04-27 23:08:24 -0400

    Fixing toggling

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -4,10 +4,14 @@ export default class extends Controller {
   static targets = [ 'comments', 'toggle', 'link' ]
 
   toggle() {
+    console.log("toggle")
+    console.log(this.toggleTarget.classList)
     if(this.toggleTarget.classList.toggle("open")) {
+      console.log("Set open")
       this.linkTarget.innerHTML = `[-]`
       this.commentsTarget.style = ""
     } else {
+      console.log("Set closed")
       this.linkTarget.innerHTML = `[+] ${ this.commentsLabel() } collapsed`
       this.commentsTarget.style = "display: none;"
     }
