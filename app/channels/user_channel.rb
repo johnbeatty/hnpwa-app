e65ce8300b4dcb2e5abe042cb53c1fa1291819b4
John Beatty <john@thebeattys.org>
2019-05-06 08:54:45 -0400

    Updating user design

--- app/channels/user_channel.rb
+++ app/channels/user_channel.rb
@@ -1,8 +1,11 @@
 class UserChannel < ApplicationCable::Channel
   def subscribed
+    stop_all_streams
     stream_from "UserChannel#{params[:user_id]}"
+    LoadUserDetailsJob.perform_later @user_id
   end
 
   def unsubscribed
+    stop_all_streams
   end
 end
