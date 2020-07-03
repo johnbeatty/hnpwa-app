960af937a4d2f27e57fa33fc0cc95d35be12a6c4
John Beatty <john@thebeattys.org>
2020-07-02 20:38:01 -0400

    Slowing down background jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,11 @@
 env :PATH, ENV['PATH']
 
-every 2.minute do
+every 5.minute do
   runner 'LoadTopItemsJob.perform_later'
   runner 'LoadNewItemsJob.perform_later'
 end
 
-every 10.minute do 
+every 20.minute do
   runner 'LoadShowItemsJob.perform_later'
   runner 'LoadJobItemsJob.perform_later'
   runner 'LoadAskItemsJob.perform_later'
