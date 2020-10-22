a482bbc404089920c5679088e35af533417502ca
John Beatty <john@thebeattys.org>
2020-10-22 09:40:26 -0400

    Updating deploy.rb with capistrano version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "~> 3.14.1"
 
 set :application, "hnpwa"
 set :repo_url, "git@github.com:johnbeatty/hnpwa-app.git"
