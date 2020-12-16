0ab635f09147f5bf2b33ccd8ea674583b1f82dfb
John Beatty <john@thebeattys.org>
2020-12-16 15:14:39 -0500

    Updating comments and toggle
    to Stimulus 2.0

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -1,38 +1,36 @@
-import { Controller } from "stimulus"
+import { Controller } from "stimulus";
 import createChannel from "cables/cable";
 
 export default class extends Controller {
-
-  static targets = ["comments"]
+  static targets = ["comments"];
 
   initialize() {
     let thisController = this;
-    this.thisChannel = createChannel( "CommentsChannel", {
+    this.thisChannel = createChannel("CommentsChannel", {
       connected() {
-        thisController.listen()
+        thisController.listen();
       },
       received({ comments, parent_id, item_id }) {
-        if (thisController.data.get('hn-id') == item_id ) {
-          thisController.commentsTarget.innerHTML = comments
+        if (thisController.data.get("hn-id") == item_id) {
+          thisController.commentsTarget.innerHTML = comments;
         }
-      }
+      },
     });
   }
 
   connect() {
-    this.listen()
+    this.listen();
   }
 
   disconnect() {
     if (this.thisChannel) {
-      this.thisChannel.perform('unfollow')
+      this.thisChannel.perform("unfollow");
     }
   }
 
   listen() {
-    
     if (this.thisChannel.consumer.connection.isOpen()) {
-      this.thisChannel.perform('follow', { parent_id: this.data.get('hn-id') } )
+      this.thisChannel.perform("follow", { parent_id: this.data.get("hn-id") });
     }
   }
 }
--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,28 +1,25 @@
-import { Controller } from "stimulus"
+import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = [ 'comments', 'toggle', 'link' ]
-
+  static targets = ["comments", "toggle", "link"];
+  static classes = ["open"];
+  static values = { count: Number };
   toggle() {
-    console.log("toggle")
-    console.log(this.toggleTarget.classList)
-    if(this.toggleTarget.classList.toggle("open")) {
-      console.log("Set open")
-      this.linkTarget.innerHTML = `[-]`
-      this.commentsTarget.style = ""
+    if (this.toggleTarget.classList.toggle(this.openClass)) {
+      this.linkTarget.innerHTML = `[-]`;
+      this.commentsTarget.style = "";
     } else {
-      console.log("Set closed")
-      this.linkTarget.innerHTML = `[+] ${ this.commentsLabel() } collapsed`
-      this.commentsTarget.style = "display: none;"
+      this.linkTarget.innerHTML = `[+] ${this.commentsLabel()} collapsed`;
+      this.commentsTarget.style = "display: none;";
     }
   }
 
   commentsLabel() {
-    let count = this.data.get('count');
-    if ( count == 1 ) {
-      return '1 reply';
+    let count = this.countValue;
+    if (count == 1) {
+      return "1 reply";
     } else {
-      return `${ count } replies`;
+      return `${count} replies`;
     }
   }
 }
