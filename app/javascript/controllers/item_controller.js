9714cd7c85fe0ce8823623ab6b6e6335a606e1ea
John Beatty <john@thebeattys.org>
2020-12-16 15:04:31 -0500

    Adding a comments loading progress indicator

--- app/javascript/controllers/item_controller.js
+++ app/javascript/controllers/item_controller.js
@@ -1,37 +1,48 @@
-import { Controller } from "stimulus"
+import { Controller } from "stimulus";
 import createChannel from "cables/cable";
 
 export default class extends Controller {
-  static targets = [ 'metadata', 'commentsHeader' ]
+  static targets = ["metadata", "commentsHeader", "progress"];
+  static values = { id: String };
 
   initialize() {
     let thisController = this;
-    this.thisChannel = createChannel( { channel: "ItemChannel",  }, {
-      connected() {
-        thisController.loadDetails()
-      },
-      received({ item, comments_header, item_id }) {
-        if ( thisController.data.get("id") == item_id ) {
-          thisController.metadataTarget.innerHTML = item; 
-          thisController.commentsHeaderTarget.innerHTML = comments_header;
-        }
+    this.thisChannel = createChannel(
+      { channel: "ItemChannel" },
+      {
+        connected() {
+          thisController.loadDetails();
+        },
+        received({ item, comments_header, progress, item_id }) {
+          if (thisController.idValue == item_id) {
+            if (item) {
+              thisController.metadataTarget.innerHTML = item;
+            }
+            if (comments_header) {
+              thisController.commentsHeaderTarget.innerHTML = comments_header;
+            }
+            if (progress) {
+              thisController.progressTarget.value = progress;
+            }
+          }
+        },
       }
-    });
+    );
   }
 
   connect() {
-    this.loadDetails()
+    this.loadDetails();
   }
 
   disconnect() {
     if (this.thisChannel) {
-      this.thisChannel.perform('unfollow')
+      this.thisChannel.perform("unfollow");
     }
   }
 
   loadDetails() {
     if (this.thisChannel) {
-      this.thisChannel.perform('follow', { id: this.data.get("id") } )
+      this.thisChannel.perform("follow", { id: this.idValue });
     }
   }
 }
--- app/workers/load_item_details_worker.rb
+++ app/workers/load_item_details_worker.rb
@@ -2,43 +2,56 @@ class LoadItemDetailsWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'comments', lock: :until_and_while_executing
 
+  
   def perform(hn_id)
-    item = Item.find_by_hn_id hn_id
+    @item = Item.find_by_hn_id hn_id
 
     begin
       http = HTTP.persistent "https://hacker-news.firebaseio.com"
-      item_json = JSON.parse http.get("/v0/item/#{item.hn_id}.json").to_s
+      item_json = JSON.parse http.get("/v0/item/#{@item.hn_id}.json").to_s
       if item_json.nil?
         return
       end
-      item.populate(item_json)
-      item.save
-
-      load_kids(http, item.hn_id, item_json)
-
+      @item.populate(item_json)
+      @item.save
+      
+      puts "total descendants #{@item.descendants}"
+      @count = 0
+      load_kids(http, @item.hn_id, item_json)
+      puts "total count #{@count}"
     ensure
       http.close if http
     end
-    item.touch
-    if item.story?
-      ActionCable.server.broadcast "ItemChannel:#{item.hn_id}", {
-        item: ItemsController.render( partial: 'item', locals: {item: item} ).squish,
-        comments_header: ItemsController.render( partial: 'comments_header', locals: {item: item, completed: true} ).squish,
-        item_id: item.hn_id
+    @item.touch
+    if @item.story?
+      ActionCable.server.broadcast "ItemChannel:#{@item.hn_id}", {
+        item: ItemsController.render( partial: 'item', locals: {item: @item} ).squish,
+        comments_header: ItemsController.render( partial: 'comments_header', locals: {item: @item, completed: true} ).squish,
+        progress: nil,
+        item_id: @item.hn_id
       }
-      ActionCable.server.broadcast "ItemsListChannel:#{item.id}", {
-        item: ItemsController.render( item ).squish,
-        item_id: item.id
+      ActionCable.server.broadcast "ItemsListChannel:#{@item.id}", {
+        item: ItemsController.render( @item ).squish,
+        item_id: @item.id
       }
-      ActionCable.server.broadcast "CommentsChannel:#{item.hn_id}", {
-        comments: ItemsController.render( partial: 'comments', locals: {item: item} ).squish,
-        parent_id: item.hn_id,
-        item_id: item.hn_id
+      ActionCable.server.broadcast "CommentsChannel:#{@item.hn_id}", {
+        comments: ItemsController.render( partial: 'comments', locals: {item: @item} ).squish,
+        parent_id: @item.hn_id,
+        item_id: @item.hn_id
       }
     end
   end
 
   def load_kids(http, parent_id, item_json)
+    @count += 1
+    puts "load kids start #{@count}"
+    ActionCable.server.broadcast "ItemChannel:#{@item.hn_id}", {
+      item: nil,
+      comments_header: nil,
+      progress: [((@count.to_f/ [@item.descendants.to_f, 1].max ) * 100).to_i, 99].min,
+      item_id: @item.hn_id
+    }
+    
     if item_json and item_json.has_key? 'kids'
       item_json['kids'].each_with_index do |kid_hn_id, kid_location|
 
@@ -52,9 +65,10 @@ class LoadItemDetailsWorker
         kid.parent_id = parent_id
         kid.populate(kid_json)
         kid.save
-
+        
         load_kids(http, kid.hn_id, kid_json)
       end
     end
+    puts "load kids stop #{@count}"
   end
 end
